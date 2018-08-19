83059c7465c25402ba52b252cfe48b8c2be66dce
Pushpendu <contactpushpendu@gmail.com>
2018-08-19 11:37:07 +0530

    http to https

--- chromosome-web/src/app/urlConfig.ts
+++ chromosome-web/src/app/urlConfig.ts
@@ -1,5 +1,5 @@
 
-export const APIURL = "http://34.216.231.223:5051/api/";
+export const APIURL = "https://server.snapbase.online/api/";
 
 // export const FILEURL ="https://api.meme.care/file/fileUpload";
 
